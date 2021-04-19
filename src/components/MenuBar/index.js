9024eee2c499b75f1fd81843c4d8a69449102f07
Henry Quinn <henryquinn@hey.com>
2021-04-19 12:50:12 -0400

    Temporarily adding Product Hunt link.

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -14,6 +14,7 @@ export default () => ( <div>
     <Link to="/patrons/">[ Patrons ]</Link>
     <br />
     --------------------------------------------<br />
+    <a href="https://www.producthunt.com/posts/henry-gives-coffee?utm_source=badge-review&utm_medium=badge&utm_souce=badge-henry-gives-coffee#discussion-body" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/review.svg?post_id=292280&theme=light" alt="Henry Gives Coffee - Micro-Scholarships for Rad. Web. Shit. | Product Hunt" width="250" height="54" /></a>
     </div>
   </div>
 )
