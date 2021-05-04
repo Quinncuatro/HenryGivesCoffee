780b0da25afa16c6fbd646caddc28f7ae01942fe
Henry Quinn <henryquinn@hey.com>
2021-05-03 21:26:03 -0400

    Updating footer to include used technologies.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,7 +9,12 @@ export default () => (
   <div>
     <NewsletterForm />
     <hr />
-    <p>Made with <span className={footerStyles.heart}>&#10084;</span> in New Haven || &copy; {year}, <a href="https://henryneeds.coffee" target="_blank" rel="noopener noreferrer">Henry Quinn</a> || <a href="https://twitter.com/henrygivescoffe" target="_blank" rel="noopener noreferrer">Twitter</a></p>
+    <div>
+      <p>Made with &#9749; in New Haven || &copy; {year}, <a href="https://henryneeds.coffee" target="_blank" rel="noopener noreferrer">Henry Quinn</a> || <a href="https://twitter.com/henrygivescoffe" target="_blank" rel="noopener noreferrer">Twitter</a></p>
+    </div>
+    <div>
+      <a href="https://github.com/quinncuatro/henrygivescoffee">Source Code</a> Includes: <a href="https://www.gatsbyjs.com/">GatsbyJS</a>, <a href="https://www.netlify.com/">Netlify</a>, <a href="https://daringfireball.net/projects/markdown/"><span title="Be the Aaron Swartz you want to see in the world.">Markdown</span></a>
+    </div>
   </div>
 )
 
